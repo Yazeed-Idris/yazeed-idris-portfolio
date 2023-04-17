b0380770772a1bac7feec7b449316e6bc13bcff9
yazed <s201730270@kfupm.edu.sa>
2023-04-17 10:53:04 +0300

    - Changed the color style of the website.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export const Header = ({fullName, socialBadges}) => {
     if (!fullName || !socialBadges) return null;
     return (
-        <header className={`w-full px-6`}>
+        <header className={`w-full px-6 bg-secondary-contrast`}>
             <div className={`container mx-auto flex justify-between items-center py-8`}>
-                <h1 className={`text-3xl font-LilitaOne text-primary`}>{fullName}</h1>
+                <h1 className={`text-3xl font-LilitaOne text-secondary`}>{fullName}</h1>
                 <div className={`flex items-center`}>
                     {socialBadges.map((badge) => (<img className={`w-10 h-10 mx-1 cursor-pointer`} key={badge.name} src={badge.icon} alt={`${badge.name} link icon`}/>))}
                 </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,10 @@
 export const Skills = ({skillsTitle, skillsContent, skillBadges}) => {
     if (!skillsTitle || !skillsContent || !skillBadges) return null;
     return (
-        <section role={'contentinfo'} aria-label={`information about ${skillsTitle}`} className={`w-full bg-secondary-contrast px-14`}>
+        <section role={'contentinfo'} aria-label={`information about ${skillsTitle}`} className={`w-full px-14`}>
             <div className={`small-container mx-auto py-8`}>
                 <h1 className={`text-4xl text-primary font-LilitaOne`}>{skillsTitle}</h1>
-                <p className={`text-2xl text-secondary `}>{skillsContent}</p>
+                <p className={`text-2xl text-secondary`}>{skillsContent}</p>
                 <div className={`flex flex-wrap justify-center items-center`}>
                     {skillBadges.map((badge) => (<img className={`w-10 h-10 mx-1 my-2 cursor-pointer`} key={badge.name} src={badge.icon} alt={`${badge.name} link icon`}/>))}
                 </div>
