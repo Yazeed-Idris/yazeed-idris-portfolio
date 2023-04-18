943d91d6c53c59dcb5437f0245a163e38346f643
yazed <s201730270@kfupm.edu.sa>
2023-04-18 13:01:43 +0300

    - Changed all absolute paths to relative ones in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -26,17 +26,17 @@ export const data ={
     social_badges: [
         {
             name: "Email",
-            icon: "/icons/email.png",
+            icon: "./icons/email.png",
             link: "mailto:yazeed.a.idris@gmail.com?subject=[GitHub]%20Portfolio&body=Hi%20Yazeed!"
         },
         {
             name: "LinkedIn",
-            icon: "/icons/linkedin.png",
+            icon: "./icons/linkedin.png",
             link: "https://www.linkedin.com/in/yazeed-idris/"
         },
         {
             name: "GitHub",
-            icon: "/icons/github.png",
+            icon: "./icons/github.png",
             link: "https://github.com/Yazeed-Idris"
         }
     ],
@@ -44,7 +44,7 @@ export const data ={
     subtitle: "I love building responsive web applications that bring life to amazing ideas.",
     _COMMENT1: "//todo: add better description",
     description: "Driven Software Engineer with a strong background in full-stack web development, project management, and deep learning. Detail-oriented, organized and meticulous developer. Works at fast pace to meet tight deadlines. Enthusiastic team player ready to contribute to company success.",
-    picture_url: "/personal_picture.png",
+    picture_url: "./personal_picture.png",
     education_title: "KFUPM",
     education_content: "I graduated from King Fahd University of Petroleum and Minerals with a Bachelor of Software Engineering and an honorary GPA of 3.124 out of 4",
     skills_title: "My Toolbox",
@@ -57,7 +57,7 @@ export const data ={
         },
         {
             name: "HTML5",
-            icon: "/icons/html5.png",
+            icon: "./icons/html5.png",
             link: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
         },
         {
@@ -77,7 +77,7 @@ export const data ={
         },
         {
             name: "CSS3",
-            icon: "/icons/css3.svg",
+            icon: "./icons/css3.svg",
             link: "https://developer.mozilla.org/en-US/docs/Web/CSS"
         },
         {
@@ -97,7 +97,7 @@ export const data ={
         },
         {
             name: "JavaScript",
-            icon: "/icons/javascript.png",
+            icon: "./icons/javascript.png",
             link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
         },
         {
@@ -112,7 +112,7 @@ export const data ={
         },
         {
             name: "NestJS",
-            icon: "/icons/nestjs.png",
+            icon: "./icons/nestjs.png",
             link: "https://nestjs.com/"
         },
         {
@@ -143,19 +143,19 @@ export const data ={
             title: "Tanseeq",
             image_url: "/projects_assets/tanseeq/Student Graduation Plan.png",
             image_urls: [
-                "/projects_assets/tanseeq/Course Information Modal.png",
-                "/projects_assets/tanseeq/Student Graduation Plan.png",
-                "/projects_assets/tanseeq/Course Pressure.png",
-                "/projects_assets/tanseeq/Faculty Pressure.png",
-                "/projects_assets/tanseeq/Generate New Plan.png",
-                "/projects_assets/tanseeq/Generated Plan (edit).png"
+                "./projects_assets/tanseeq/Course Information Modal.png",
+                "./projects_assets/tanseeq/Student Graduation Plan.png",
+                "./projects_assets/tanseeq/Course Pressure.png",
+                "./projects_assets/tanseeq/Faculty Pressure.png",
+                "./projects_assets/tanseeq/Generate New Plan.png",
+                "./projects_assets/tanseeq/Generated Plan (edit).png"
             ],
             description: "Tanseeq is a web application that helps students plan their graduation and choose their courses. It also helps faculty members to monitor the pressure on their courses and make decisions based on that.",
             more_link: "https://docs.google.com/presentation/d/1yKt9N3Jw3c3S5lC8Ec9elELolN-5YyDt/edit?usp=share_link&ouid=101325767605015031540&rtpof=true&sd=true",
             used_technologies: [
                 {
                     name: "NestJS",
-                    icon: "/icons/nestjs.png",
+                    icon: "./icons/nestjs.png",
                     link: "https://nestjs.com/"
                 },
                 {
@@ -179,8 +179,8 @@ export const data ={
             title: "Age Calculator",
             image_url: "/projects_assets/age-calculator-app/desktop-view.png",
             image_urls: [
-                "/projects_assets/age-calculator-app/desktop-view.png",
-                "/projects_assets/age-calculator-app/mobile-view.png"
+                "./projects_assets/age-calculator-app/desktop-view.png",
+                "./projects_assets/age-calculator-app/mobile-view.png"
             ],
             description: "Age Calculator is a web application that calculates the age of a person based on their birth date. It also calculates the number of days, hours, minutes, and seconds that have passed since their birth.",
             live_site_link: "https://yazeed-idris.github.io/age-calculator-app",
@@ -210,12 +210,12 @@ export const data ={
         },
         {
             title: "Grammy",
-            image_url: "/projects_assets/grammy/grammy post.PNG",
+            image_url: "./projects_assets/grammy/grammy post.PNG",
             image_urls: [
-                "/projects_assets/grammy/grammy post.PNG",
-                "/projects_assets/grammy/grammy login.PNG",
-                "/projects_assets/grammy/grammy signup.PNG",
-                "/projects_assets/grammy/grammy create post.PNG"
+                "./projects_assets/grammy/grammy post.PNG",
+                "./projects_assets/grammy/grammy login.PNG",
+                "./projects_assets/grammy/grammy signup.PNG",
+                "./projects_assets/grammy/grammy create post.PNG"
             ],
             description: "Grammy is a social media web application that allows users to share their thoughts and ideas with others. It also allows users to follow other users and see their posts in their feed.",
             live_site_link: "https://yazeed-idris.github.io/grammy/",
@@ -240,14 +240,14 @@ export const data ={
         },
         {
             title: "Countries API",
-            image_url: "/projects_assets/rest-countries-api/desktop-dark-main.png",
+            image_url: "./projects_assets/rest-countries-api/desktop-dark-main.png",
             image_urls: [
-                "/projects_assets/rest-countries-api/desktop-dark-main.png",
-                "/projects_assets/rest-countries-api/desktop-dark-info.png",
-                "/projects_assets/rest-countries-api/desktop-light-main-filter.png",
-                "/projects_assets/rest-countries-api/desktop-dark-info.png",
-                "/projects_assets/rest-countries-api/mobile-dark-main.png",
-                "/projects_assets/rest-countries-api/mobile-dark-info.png"
+                "./projects_assets/rest-countries-api/desktop-dark-main.png",
+                "./projects_assets/rest-countries-api/desktop-dark-info.png",
+                "./projects_assets/rest-countries-api/desktop-light-main-filter.png",
+                "./projects_assets/rest-countries-api/desktop-dark-info.png",
+                "./projects_assets/rest-countries-api/mobile-dark-main.png",
+                "./projects_assets/rest-countries-api/mobile-dark-info.png"
             ],
             description: "Countries API is a web application that displays information about all the countries in the world. It also allows users to filter the countries by region and search for a specific country.",
             live_site_link: "https://yazeed-idris.github.io/rest-countries-api-with-color-theme-switcher/",
@@ -276,10 +276,10 @@ export const data ={
         },
         {
             title: "Results Summary Component",
-            image_url: "/projects_assets/results-summary-component/desktop-main.png",
+            image_url: "./projects_assets/results-summary-component/desktop-main.png",
             image_urls: [
-                "/projects_assets/results-summary-component/desktop-main.png",
-                "/projects_assets/results-summary-component/mobile-main.png"
+                "./projects_assets/results-summary-component/desktop-main.png",
+                "./projects_assets/results-summary-component/mobile-main.png"
             ],
             description: "Results Summary Component is a display component for results of any kind. It displays the total number of results, the number of results per page, and the current page number. It also allows users to navigate between pages.",
             live_site_link: "https://yazeed-idris.github.io/results-summary-component/",
@@ -287,26 +287,26 @@ export const data ={
             used_technologies: [
                 {
                     name: "HTML5",
-                    icon: "/icons/html5.png",
+                    icon: "./icons/html5.png",
                     link: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
                 },
                 {
                     name: "CSS3",
-                    icon: "/icons/css3.svg",
+                    icon: "./icons/css3.svg",
                     link: "https://developer.mozilla.org/en-US/docs/Web/CSS"
                 }
             ]
         },
         {
             title: "Rock Paper Scissors",
-            image_url: "/projects_assets/rock-paper-scissors/rock-paper-scissors-desktop.png",
+            image_url: "./projects_assets/rock-paper-scissors/rock-paper-scissors-desktop.png",
             image_urls: [
-                "/projects_assets/rock-paper-scissors/rock-paper-scissors-desktop.png",
-                "/projects_assets/rock-paper-scissors/rock-paper-scissors-mobile.png",
-                "/projects_assets/rock-paper-scissors/rock-paper-scissors-desktop-match.png",
-                "/projects_assets/rock-paper-scissors/rock-paper-scissors-mobile-match.png",
-                "/projects_assets/rock-paper-scissors/rock-paper-scissors-desktop-rules.png",
-                "/projects_assets/rock-paper-scissors/rock-paper-scissors-mobile-rules.png"
+                "./projects_assets/rock-paper-scissors/rock-paper-scissors-desktop.png",
+                "./projects_assets/rock-paper-scissors/rock-paper-scissors-mobile.png",
+                "./projects_assets/rock-paper-scissors/rock-paper-scissors-desktop-match.png",
+                "./projects_assets/rock-paper-scissors/rock-paper-scissors-mobile-match.png",
+                "./projects_assets/rock-paper-scissors/rock-paper-scissors-desktop-rules.png",
+                "./projects_assets/rock-paper-scissors/rock-paper-scissors-mobile-rules.png"
             ],
             description: "A simple rock paper scissors game built with HTML, CSS and JavaScript.",
             live_site_link: "https://yazeed-idris.github.io/rock-paper-scissors-production/",
@@ -331,14 +331,14 @@ export const data ={
         },
         {
             title: "Rock Paper Scissors Lizard Spock",
-            image_url: "/projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop.png",
+            image_url: "./projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop.png",
             image_urls: [
-                "/projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop.png",
-                "/projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-mobile.png",
-                "/projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop-match.png",
-                "/projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-mobile-match.png",
-                "/projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop-rules.png",
-                "/projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-mobile-rules.png"
+                "./projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop.png",
+                "./projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-mobile.png",
+                "./projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop-match.png",
+                "./projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-mobile-match.png",
+                "./projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-desktop-rules.png",
+                "./projects_assets/rock-paper-scissors-lizard-spock/rock-paper-scissors-lizard-spock-mobile-rules.png"
             ],
             description: "A simple rock paper scissors lizard spock game built with HTML, CSS and JavaScript.",
             live_site_link: "https://yazeed-idris.github.io/rock-paper-scissors-lizard-spock-production/",
@@ -363,16 +363,16 @@ export const data ={
         },
         {
             title: "Sales Page",
-            image_url: "/projects_assets/sales-demo/sales-demo-desktop.PNG",
+            image_url: "./projects_assets/sales-demo/sales-demo-desktop.PNG",
             image_urls: [
-                "/projects_assets/sales-demo/sales-demo-desktop.PNG",
-                "/projects_assets/sales-demo/sales-demo-mobile.PNG"
+                "./projects_assets/sales-demo/sales-demo-desktop.PNG",
+                "./projects_assets/sales-demo/sales-demo-mobile.PNG"
             ],
             description: "A simple sales page built with HTML, CSS and JavaScript.",
             used_technologies: [
                 {
                     name: "HTML5",
-                    icon: "/icons/html5.png",
+                    icon: "./icons/html5.png",
                     link: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
                 },
                 {
@@ -382,17 +382,17 @@ export const data ={
                 },
                 {
                     name: "JavaScript",
-                    icon: "/icons/javascript.png",
+                    icon: "./icons/javascript.png",
                     link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
                 }
             ]
         },
         {
             title: "Tip Calculator",
-            image_url: "/projects_assets/tip-calculator-app/desktop-main.png",
+            image_url: "./projects_assets/tip-calculator-app/desktop-main.png",
             image_urls: [
-                "/projects_assets/tip-calculator-app/desktop-main.png",
-                "/projects_assets/tip-calculator-app/mobile-main.png"
+                "./projects_assets/tip-calculator-app/desktop-main.png",
+                "./projects_assets/tip-calculator-app/mobile-main.png"
             ],
             description: "A simple tip calculator built with HTML, CSS and JavaScript.",
             live_site_link: "https://yazeed-idris.github.io/tip-calculator-app/",
