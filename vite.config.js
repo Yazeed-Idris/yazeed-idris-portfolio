b9fed87bb0604bdbcf143bb613afe1fb290ca735
yazed <s201730270@kfupm.edu.sa>
2023-04-21 18:19:19 +0300

    - Changed base url for vite and package.json. Also added CNAME file for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yazeed-idris-portfolio/",
+  base: "https://yazeed-idris.com/",
   plugins: [react()],
   publicDir: "public",
 })
